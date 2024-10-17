d51754261549aeec0937030a55c101ca89463de7
Soji <sojithedeveloper@gmail.com>
2024-10-16 23:57:48 -0500

    Initial Commit

---
+++ NetworkMonitoring/NetworkMonitoringApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkMonitoringApp.swift
+//  NetworkMonitoring
+//
+//  Created by James Afonja on 10/16/24.
+//
+
+import SwiftUI
+
+@main
+struct NetworkMonitoringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NetworkMonitoringTests/NetworkMonitoringTests.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkMonitoringTests.swift
+//  NetworkMonitoringTests
+//
+//  Created by James Afonja on 10/16/24.
+//
+
+import Testing
+@testable import NetworkMonitoring
+
+struct NetworkMonitoringTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
